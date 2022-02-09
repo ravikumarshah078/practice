b31307e2168c7f6b99cee857367f38e444c63a9e
Ravi Kumar <ravikumarshah078@gmail.com>
2022-02-09 21:43:46 +0530

    09-02-2022

--- ration_of_ele_in_arr/index.php
+++ ration_of_ele_in_arr/index.php
@@ -8,8 +8,26 @@
 
 function plusMinus($arr) {
     // Write your code here
-
+    $positive = 0;
+    $negative = 0;
+    $zero = 0;
+    $number = count($arr);
+    for($i = 0; $i<$number; $i++){
+        if($arr[$i] > 0){
+            $positive++;
+        }else if($arr[$i] < 0){
+            $negative++;
+        }else{
+            $zero++;
+        }
+    }
+    echo sprintf('%0.6f', $positive/$number)."\r\n";
+    echo sprintf('%0.6f', $negative/$number)."\r\n";
+    echo sprintf('%0.6f', $zero/$number)."\r\n";
 }
+
+plusMinus([-4, 3, -9, 0, 4, 1]);
+exit;
 
 $n = intval(trim(fgets(STDIN)));
 
